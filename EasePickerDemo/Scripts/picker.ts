feebbc5a25f9523975a6f47013ae7eee04475637
Tim Purdum <tim.purdum@dymaptic.com>
2024-04-30 19:52:50 -0500

    Starting Demo

--- EasePickerDemo/Scripts/picker.ts
+++ EasePickerDemo/Scripts/picker.ts
@@ -1,33 +1,7 @@
-﻿import { easepick, RangePlugin, AmpPlugin } from '@easepick/bundle';
+﻿import { easepick } from '@easepick/bundle';
 import { IPickerConfig } from '@easepick/core/dist/types';
 
-export async function createPicker(options: IPickerConfig, dotNetRef: any, endDateDiv: any | null) {
-    let isRangeEnabled = false;
-    if (options.plugins.length > 0) {
-        // replace string representation of plugin with actual plugin
-        options.plugins = options.plugins.map((plugin) => {
-            switch (plugin) {
-                case 'RangePlugin':
-                    isRangeEnabled = true;
-                    if (endDateDiv) {
-                        options.RangePlugin.elementEnd = endDateDiv;
-                    }
-                    return RangePlugin;
-                case 'AmpPlugin':
-                    if ((options.AmpPlugin.dropdown as any).yearsAscending) {
-                        options.AmpPlugin.dropdown.years = 'asc';
-                    }
-                    return AmpPlugin;
-            }
-        });
-    }
+export async function createPicker(options: IPickerConfig) {
     const picker = new easepick.create(options);
-    picker.on('select', async (e) => {
-        if (isRangeEnabled) {
-            await dotNetRef.invokeMethodAsync('OnDateRangeSelected', e.detail.start, e.detail.end);
-        } else {
-            await dotNetRef.invokeMethodAsync('OnDateSelected', e.detail.date);
-        }
-    });
     return picker;
 }
