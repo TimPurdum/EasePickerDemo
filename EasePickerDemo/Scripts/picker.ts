de561c6b5eabb5099bce4f27bf96c7d31ab51fed
Tim Purdum <tim.purdum@dymaptic.com>
2024-09-25 21:13:18 -0400

    update package

--- EasePickerDemo/Scripts/picker.ts
+++ EasePickerDemo/Scripts/picker.ts
@@ -1,10 +1,9 @@
 ﻿import { easepick } from '@easepick/bundle';
 import { IPickerConfig } from '@easepick/core/dist/types';
 
-export async function createPicker(options: IPickerConfig, endDateDiv: any | null,
-    dotNetRef: any) {
+export async function createPicker(options: IPickerConfig) {
 
     const picker = new easepick.create(options);
                     
     return picker;
-}          
+}
